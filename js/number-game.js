e752a0e1b8e9bd29fa4b90904bbd11c9c4460339
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-13 13:24:16 -0700

    checkGuess function

--- js/number-game.js
+++ js/number-game.js
@@ -7,8 +7,24 @@ function runGame() {
 
   do {
     guessString = prompt("Enter a number between 1 and 100");
-    numTries++
+    numTries++;
     guessNumber = +guessString;
-    correct = checkGuess(guessNumber, target)
+    correct = checkGuess(guessNumber, target);
   } while (!correct);
+}
+
+function checkGuess(guessNumber, target) {
+  let correct = false;
+  if (isNaN(guessNumber)) {
+    alert("The value entered must be a number");
+  } else if (guessNumber < 1 || guessNumber > 100) {
+    alert("The number entered must be from 1-100");
+  } else if (guessNumber > target) {
+    alert("The number you entered is larger than the target.");
+  } else if (guessNumber < target) {
+    alert("The number you entered is less than the target.");
+  } else {
+      correct = true;
+  }
+  return correct;
 }
