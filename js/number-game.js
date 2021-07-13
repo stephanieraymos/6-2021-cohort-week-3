be2402c3ab35c42e0eccb1e1581518c4023f6988
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-13 11:58:48 -0700

    runGame function

--- js/number-game.js
+++ js/number-game.js
@@ -1,3 +1,14 @@
 function runGame() {
-    
+  target = Math.floor(Math.random() * 100) + 1;
+  let guessString = "";
+  let guessNumber = 0;
+  let correct = false;
+  let numTries = 0;
+
+  do {
+    guessString = prompt("Enter a number between 1 and 100");
+    numTries++
+    guessNumber = +guessString;
+    correct = checkGuess(guessNumber, target)
+  } while (!correct);
 }
